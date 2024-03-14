7d61de70296f149ecd456002c8ae4b1484a292bb
Diyana Cholakova <didkacholakova999@gmail.com>
2024-03-14 15:10:19 +0200

    added user service with dummy login ang logout

---
+++ rock-hunters/src/app/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user: User | undefined;
+  USER_KEY = '[user]';
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (err) {
+      this.user = undefined;
+      // localStorage.removeItem(this.USER_KEY);
+    }
+  }
+
+  login() {
+    this.user = {
+      email: 'pinko@panther.pink',
+      username: 'Pinko',
+      password: '123'
+    }
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
+}
