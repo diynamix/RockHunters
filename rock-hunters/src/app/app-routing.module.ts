b449de948f668a7949bdc1b8d351350e66d7b236
Diyana Cholakova <didkacholakova999@gmail.com>
2024-03-14 12:14:54 +0200

    error page

--- rock-hunters/src/app/app-routing.module.ts
+++ rock-hunters/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
-  // { path: '**', redirectTo: '/home' },
+  { path: '**', redirectTo: '/error' },
+  { path: 'error', component: ErrorComponent },
 ];
 
 @NgModule({
--- rock-hunters/src/app/app.module.ts
+++ rock-hunters/src/app/app.module.ts
@@ -8,19 +8,22 @@ import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserModule } from './user/user.module';
 import { RockModule } from './rock/rock.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CoreModule,
     HttpClientModule,
     UserModule,
     RockModule,
+    // AppRoutingModule stay at bottom
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
