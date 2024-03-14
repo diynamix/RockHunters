3d9e1b88f07746234d5c8d13ae980077b2ad54df
Diyana Cholakova <didkacholakova999@gmail.com>
2024-03-14 13:51:35 +0200

    added rock-add page

--- rock-hunters/src/app/rock/rock-routing-module.ts
+++ rock-hunters/src/app/rock/rock-routing-module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { RockAddComponent } from "./rock-add/rock-add.component";
 import { RockDetailsComponent } from "./rock-details/rock-details.component";
 import { RockListComponent } from "./rock-list/rock-list.component";
 
@@ -7,6 +8,8 @@ import { RockListComponent } from "./rock-list/rock-list.component";
 const routes: Routes = [
     { path: 'rocks', children: [
         { path: '', pathMatch: 'full', component: RockListComponent },
+        { path: 'add', component: RockAddComponent },
+        // RockDetails path MUST stay after RockAdd path
         { path: ':rockId', component: RockDetailsComponent },
     ],
     },
