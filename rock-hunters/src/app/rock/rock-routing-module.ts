a552ad6cd17936ded7937f649e99cfd6d6947fac
Diyana Cholakova <didkacholakova999@gmail.com>
2024-03-16 21:46:03 +0200

    added auth route guard

--- rock-hunters/src/app/rock/rock-routing-module.ts
+++ rock-hunters/src/app/rock/rock-routing-module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthActivate } from "../guards/auth.activate";
 import { RockAddComponent } from "./rock-add/rock-add.component";
 import { RockDetailsComponent } from "./rock-details/rock-details.component";
 import { RockListComponent } from "./rock-list/rock-list.component";
@@ -7,10 +8,21 @@ import { RockListComponent } from "./rock-list/rock-list.component";
 
 const routes: Routes = [
     { path: 'rocks', children: [
-        { path: '', pathMatch: 'full', component: RockListComponent },
-        { path: 'add', component: RockAddComponent },
+        {
+            path: '',
+            pathMatch: 'full',
+            component: RockListComponent
+        },
+        {
+            path: 'add',
+            component: RockAddComponent,
+            canActivate: [AuthActivate]
+        },
         // RockDetails path MUST stay after RockAdd path
-        { path: ':rockId', component: RockDetailsComponent },
+        {
+            path: ':rockId',
+            component: RockDetailsComponent
+        },
     ],
     },
 ];
